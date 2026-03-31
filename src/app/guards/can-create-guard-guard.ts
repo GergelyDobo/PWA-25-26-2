043b3fb9e6adee3eb7029f9f80c127b1b27d7889
gergely.dobo <gergely.dobo@casrd.net>
2026-03-31 13:21:35 +0200

    Always allow to create buildings

--- src/app/guards/can-create-guard-guard.ts
+++ src/app/guards/can-create-guard-guard.ts
@@ -7,13 +7,18 @@ export const canCreateGuard: CanActivateFn = (route, state) => {
   const mgmtService = inject(ManagementService);
   const router = inject(Router);
   let totalBuildings = 0;
+
   mgmtService.getBuildings().forEach((building: Building) => {
     totalBuildings += building.amountPurchased;
   })
+
+  /*
   if(totalBuildings < 2) {
     alert('You need at least 2 buildings to be able to create a new one!');
     router.navigateByUrl('home');
     return false;
   }
+  */
+
   return true;
 };
